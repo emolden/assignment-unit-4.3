a5a91fa79a9653e08dd5a43386234b19edd1f746
Emma Molden <emma.molden007@gmail.com>
2024-06-28 20:06:25 -0500

    required code complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,62 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//Create a global variable named basket and set it to an empty array.
+
+let basket = [];
+
+//Create a function called addItem. It should:
+    //take an input parameter for a string item
+    //add the new item to the global array basket.
+    //return true indicating the item was added
+
+function addItem (item) {
+    basket.push(item);
+    return true;
+}
+let one = 'one';
+let two = 'two';
+let three = 'three';
+
+addItem(one);
+addItem(two);
+addItem(three);
+
+console.log('basket items', basket);
+
+//Create a function called listItems. It should:
+    //loop over the items in the basket array
+    //console.log each individual item on a new line
+
+function listItems(list) {
+    for (i = 0; i<list.length; i++) {
+        console.log(list[i]);
+    }
+    return list;
+}
+
+let basketTest = listItems(basket);
+
+console.log('Test - listItems:', basketTest);
 
 
+//Create a function called empty. It should:
+    //reset the basket to an empty array
+    //do not use basket = [] to reset the array
+function empty (arrayToEmpty) {
+    if (arrayToEmpty === []){
 
-
-
-
-
-
-
+    }
+    else{
+        for (i=0; i<arrayToEmpty.length; i++) {
+            arrayToEmpty.shift();
+         i--;
+        }
+    }
+    return arrayToEmpty;
+}
+let emptyBasket = empty(basket);
+console.log('emptying basket:', emptyBasket);
 
 // DO NOT MODIFY
 // Used for automated testing
